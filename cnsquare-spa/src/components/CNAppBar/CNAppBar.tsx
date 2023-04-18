52c6919ca549f617f3480a9af87577ac8cbd5896
kwokkayan <kwokkayan0712@gmail.com>
2023-04-19 01:41:15 +0800

    fix: I give up on react

--- cnsquare-spa/src/components/CNAppBar/CNAppBar.tsx
+++ cnsquare-spa/src/components/CNAppBar/CNAppBar.tsx
@@ -18,10 +18,11 @@ function CNAppBar() {
   const { user, cart } = useContext(UserContext);
   const [quantity, setQuantity] = useState<number>(0);
   useEffect(() => {
+    const c = JSON.parse(window.localStorage.getItem("cart") as string) as any[];
     setQuantity(
-      cart === null
+      c === null
         ? 0
-        : cart.reduce<number>((prev, curr) => prev + Number(curr.quantity), 0)
+        : c.reduce<number>((prev, curr) => prev + Number(curr.quantity), 0)
     );
   }, [cart]);
   const navigate = useNavigate();
