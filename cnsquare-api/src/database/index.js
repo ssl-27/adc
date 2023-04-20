365db687194a3b7016a891a6a4be4d2e48241571
Terry Leung <59078445+terrylcc@users.noreply.github.com>
2023-04-20 22:54:44 +0800

    feat: add db.json with custom product data

--- cnsquare-api/src/database/index.js
+++ cnsquare-api/src/database/index.js
@@ -1,8 +1,9 @@
 const config = require("../config");
 const faker = require("./faker");
+const db = require("./db.json");
 
 exports.createData = () => {
-  const data = { users: [], orders: [], products: [], reviews: [] };
+  const data = { users: [], orders: [], products: db.products, reviews: [] };
 
   for (let i = 1; i <= config.NUMBER_OF_USERS; i++) {
     data.users.push(faker.createUser(i));
@@ -12,9 +13,9 @@ exports.createData = () => {
     data.orders.push(faker.createOrder(i));
   }
 
-  for (let i = 1; i <= config.NUMBER_OF_PRODUCTS; i++) {
-    data.products.push(faker.createProduct(i));
-  }
+  // for (let i = 1; i <= config.NUMBER_OF_PRODUCTS; i++) {
+  //   data.products.push(faker.createProduct(i));
+  // }
 
   for (let i = 1; i <= config.NUMBER_OF_REVIEWS; i++) {
     data.reviews.push(faker.createReview(i));
